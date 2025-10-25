6079cb8651b1cc6604c6161bbef323f351a13c9d
Sneara0 <snearaparvin.cse@gmail.com>
2025-10-25 21:59:14 +0600

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+"use client";
+ import { useState } from "react";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
+import { LuSunMoon } from "react-icons/lu";
+import { IoMdMoon } from "react-icons/io";
+import Link from "next/link";
+
+
+export default function Navbar() {
+
+
+    const [menuOpen, setMenuOpen]=useState(false);
+    const [darkMode, setDarkMode]=useState(false);
+
+    return(
+
+    )
+}
